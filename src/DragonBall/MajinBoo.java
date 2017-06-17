8f33de1cae7a7ce33bbcd297e6e40e62b05bf756
Ariii1996 <ariel.piro@hotmail.com>
2017-06-17 13:40:19 -0300

    A

--- src/DragonBall/MajinBoo.java
+++ src/DragonBall/MajinBoo.java
@@ -32,7 +32,7 @@ public class MajinBoo extends Personaje{
 	
 	public void ataqueEspecial(Personaje enemigo) throws IncapacidadDeAtacar, InhabilitadoError{
 		
-		
+    	if (!equipo.esMiTurno()) throw new IncapacidadDeAtacar();
 		if (this.estaInmovilizado()) throw new InhabilitadoError();
 		if (this.tablero.estanARangoDeAtaque(this, enemigo) && this.ki >= this.costo_ataque_especial && !equipo.ataco()){
 			if (equipo.seEstaMoviendo()) this.equipo.moverseYAtacar();
