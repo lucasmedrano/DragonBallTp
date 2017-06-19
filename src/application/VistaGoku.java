f2b383252e53d64b43ef8069a84776c0fd42bc09
Ariii1996 <ariel.piro@hotmail.com>
2017-06-19 11:22:26 -0300

    A

--- src/application/VistaGoku.java
+++ src/application/VistaGoku.java
@@ -26,9 +26,7 @@ public class VistaGoku {
 
     private void dibujarFormas() {
     	Image imagen = new Image(goku.delvolverRutaNormal());
-    	canvas.getGraphicsContext2D().drawImage(imagen, 1, 1, 75, 75);
-        //canvas.getGraphicsContext2D().setFill(Color.DARKBLUE);
-        canvas.getGraphicsContext2D().fillOval(goku.obtenerUbicacion().obtenerUbicacionHorizontal() + 20 + this.x, goku.obtenerUbicacion().obtenerUbicacionVertical() + 500 + this.y, 30, 30);
+    	canvas.getGraphicsContext2D().drawImage(imagen, goku.obtenerUbicacion().obtenerUbicacionHorizontal() + 20 + this.x, goku.obtenerUbicacion().obtenerUbicacionVertical() + 500 + this.y, 75, 75);
     }
 
     public void update(int x, int y) {
