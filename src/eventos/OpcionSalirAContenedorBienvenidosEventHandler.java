855837e8cfe0d9be106fe668888a7c85de97df53
Lucas Nicolas Medrano <lucasmedrano97@gmail.com>
2017-06-20 00:37:16 -0300

    clase para boton reiniciar.

---
+++ src/eventos/OpcionSalirAContenedorBienvenidosEventHandler.java
@@ -0,0 +1,64 @@
+package eventos;
+
+import DragonBall.Cell;
+import DragonBall.Equipo;
+import DragonBall.Freezer;
+import DragonBall.Gohan;
+import DragonBall.Goku;
+import DragonBall.Juego;
+import DragonBall.Jugador;
+import DragonBall.MajinBoo;
+import DragonBall.Piccolo;
+import DragonBall.Tablero;
+import application.Batalla;
+import application.ContenedorBienvenidos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class OpcionSalirAContenedorBienvenidosEventHandler implements EventHandler<ActionEvent>{
+	
+	Stage stage;
+	public OpcionSalirAContenedorBienvenidosEventHandler(Stage stage) {
+		this.stage = stage;
+	}
+	
+	@Override
+	public void handle(ActionEvent actionEvent) {
+		Juego juego = new Juego();
+		Tablero tablero = juego.obtenerTablero();
+		Goku goku = new Goku(tablero);
+		Piccolo piccolo = new Piccolo(tablero);
+		Gohan gohan = new Gohan(tablero);
+		Freezer freezer = new Freezer(tablero);
+		Cell cell = new Cell(tablero);
+		MajinBoo boo = new MajinBoo(tablero);
+		Jugador jugador1 = juego.obtenerJugador1();
+		Jugador jugador2 = juego.obtenerJugador2();
+		Equipo equipo1 = new Equipo();
+		equipo1.asignarPersonajes(gohan,goku, piccolo);
+		Equipo equipo2 = new Equipo();
+		equipo2.asignarPersonajes(freezer, cell, boo);
+		
+
+		jugador1.asignarEquipo(equipo1);
+		jugador2.asignarEquipo(equipo2);
+		
+		tablero.ubicarPersonajesJugador1(jugador1);
+		tablero.ubicarPersonajesJugador2(jugador2);
+		
+		juego.empiezaJuego();
+		Batalla batalla = new Batalla(stage, goku, gohan, piccolo, freezer, cell, boo, juego);
+
+		Scene scene = new Scene(batalla, 30, 30);
+		ContenedorBienvenidos contenedorBienvenidos = new ContenedorBienvenidos(stage, scene);
+		Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 800, 600);
+		
+		stage.setScene(escenaBienvenidos);
+		stage.setFullScreen(true);
+		
+		stage.show();
+	}
+
+}
